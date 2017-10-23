68136cdf834cd6b557d8e1656651db2754de2f40
Jason-Calderone <jcalderone_13@yahoo.com>
2017-10-23 19:04:42 -0400

    Homework-6

---
+++ Questions 1-3/Main.js
@@ -0,0 +1,33 @@
+
+//Question-1. Part A
+var a = 5, b = 4, s = a + b;
+
+var sum = a + b;
+
+console.log (a + b);
+
+
+//Part B
+var c = "four", d = " five";
+
+console.log (c + d)
+
+
+//Question-2
+
+var sports = [["Yanks", "Mets", "Red-Sux"] , ["Knicks", "Nets", "Mavs"] , ["Giants" , "Jets" , "NE-Putz"]];
+
+console.log (sports, [0,2][0,1][2,1]);
+
+//Question-3
+
+var number = prompt("Please enter a number")
+
+if (number < 100) {
+    alert("the variable is less than 100");
+} else {
+    alert("the variable is greater than 100");
+};
+
+//Question-4
+
